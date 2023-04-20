ec80ec2400a9d27a44ab1e3d7f16642bea210ebc
Lennon Nagata <lennon_nagata@hotmail.com>
2023-04-20 17:27:05 -0300

    atualizado codigos

--- src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.AvaliacaoFisica;
 import me.dio.academia.digital.entity.form.AlunoForm;
 import me.dio.academia.digital.service.impl.AlunoServiceImpl;
 
@@ -22,11 +24,16 @@ public class AlunoController {
 
     @GetMapping
     public List<Aluno> getAll() {
-        return service.getAll();        
+        return service.getAll();
     }
 
     @PostMapping
-    public Aluno create(@RequestBody AlunoForm form){
+    public Aluno create(@RequestBody AlunoForm form) {
         return service.create(form);
+    }
+
+    @GetMapping("/avaliacoes/{id}")
+    public List<AvaliacaoFisica> getAllAvaliacaoFisicaId(@PathVariable Long id) {
+        return service.getAllAvaliacaoFisicaId(id);
     }
 }
