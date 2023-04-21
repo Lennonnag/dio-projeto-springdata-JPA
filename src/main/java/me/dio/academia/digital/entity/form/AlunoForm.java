e9cecfed3b135806ef7add748c2e967600b50757
Lennon Nagata <lennon_nagata@hotmail.com>
2023-04-21 12:00:13 -0300

    adicionado validações

--- src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
@@ -2,6 +2,11 @@ package me.dio.academia.digital.entity.form;
 
 import java.time.LocalDate;
 
+import org.hibernate.validator.constraints.br.CPF;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,11 +16,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AlunoForm {
 
-  private String nome;
-
-  private String cpf;
-
-  private String bairro;
-
+ @NotBlank(message = "Preencha o campo corretamente.")
+ @Size(min = 3, max = 50, messagem = "'${validatedValue}' precisa estar entre {min} e {max} caracteres.")
+ private String nome;
+ 
+ @NotBlank(message = "Preencha o campo corretamente.")
+ @CPF(message = "'${validatedValue}' é inválido!")
+ private String cpf;
+ 
+ @NotBlank(message = "Preencha o campo corretamente.")
+ @Size(min = 3, max = 50, messagem = "'${validateValue}' precisa estar entre {min} e {max} caracteres.")
+ private String bairro;
+ 
+ @NotBlank(message = "Preencha o campo corretamente.")
+ @Past(message = "Data '${validatedValue}' é inválida.")
   private LocalDate dataDeNascimento;
 }
